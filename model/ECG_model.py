c512c55f89c2282e2283cf7b60f04647fcf7b94d
kknankk <167001111+kknankk@users.noreply.github.com>
2024-10-03 00:44:38 +0800

    Update ECG_model.py

--- model/ECG_model.py
+++ model/ECG_model.py
@@ -243,3 +243,95 @@ class ResNet1d(nn.Module):
         return x
 
 
+#-------------------frequency domain model
+#adaptive from https://github.com/UARK-AICV/ECG_SSL_12Lead/blob/main/models/seresnet2d.py#L112
+
+from torchvision.models import ResNet
+from torch import nn
+
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            # nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+class SEBasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None,
+                 *, reduction=16):
+        super(SEBasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes, 1)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.se = SELayer(planes, reduction)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.se(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+def se_resnet34(num_classes=7):
+    """Constructs a ResNet-34 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(SEBasicBlock, [3, 4, 6, 3], num_classes=num_classes)
+    model.avgpool = nn.AdaptiveAvgPool2d(1)
+    return model
+
+
+
+class spectrogram_model(nn.Module):
+    def __init__(self,num_classes=7):
+        super(spectrogram_model,self).__init__()
+        self.backbone = se_resnet34()
+        self.backbone.conv1 = nn.Conv2d(12, 64, kernel_size=7, stride=2, padding=3)
+        list_of_modules = list(self.backbone.children())
+        self.features = nn.Sequential(*list_of_modules[:-1])
+        num_ftrs = self.backbone.fc.in_features
+    
+        self.fc = nn.Sequential(
+                nn.Linear(in_features=num_ftrs,out_features=num_ftrs//2),
+                nn.Linear(in_features=num_ftrs//2,out_features=num_classes)
+            )
+
+    def forward(self, x):
+        h = self.features(x)
+        h1 = h.squeeze()
+        x = self.fc(h1)
+        return h1,x
+
